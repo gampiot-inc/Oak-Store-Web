9eb482f507fd9e98d5fbccefbb695d83d60cc459
aquilesTrindade <sempedra075@gmail.com>
2024-06-12 02:31:50 -0300

    More improvements

--- website/trindade/dev/js/oak/home/home.js
+++ website/trindade/dev/js/oak/home/home.js
@@ -1,7 +1,11 @@
-const url = '../../../../../aglsk'
+const username = "oak-store";
+const repo = "Oak-Web/";
+const usermod = "aglsk/";
+const baseUrl = "https://" + username + ".github.io/" + repo + usermod + "?id=";
+const url = '../../../../../aglsk/?id=';
 
 function detailsApp(id) {
-  open(url + `/?id=${id}`);
+  open(url + id);
 }
 
 function open(uri) {
