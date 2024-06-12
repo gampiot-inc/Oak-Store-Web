beea138d5132575194e993f8e86d53470ff36ec3
aquilesTrindade <sempedra075@gmail.com>
2024-06-12 03:00:56 -0300

    More improvements

--- website/trindade/dev/js/oak/home/home.js
+++ website/trindade/dev/js/oak/home/home.js
@@ -1,13 +1,3 @@
-const username = "aquilestrindade";
-const repo = "Oak-Web/";
-const usermod = "aglsk/";
-const baseUrl = "https://" + username + ".github.io/" + repo + usermod + "?id=";
-const url = './website/aglsk/?id=';
-
 function detailsApp(id) {
-  open(baseUrl + id);
-}
-
-function open(uri) {
-  window.location.href = uri;
+   window.location.href = ./redirector.html;
 }
---
+++ website/trindade/dev/js/oak/home/uz.js
@@ -0,0 +1,12 @@
+
+const username = "aquilestrindade";
+const repo = "Oak-Web/";
+const usermod = "aglsk/";
+const baseUrl = "https://" + username + ".github.io/" + repo + usermod + "?id=";
+const url = './website/aglsk/?id=';
+
+
+function open(uri) {
+   window.location.href = uri;
+}
+open(baseUrl + id);
