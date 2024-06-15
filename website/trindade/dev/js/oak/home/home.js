7634fab955685a4db2acfaa73e157b77a9cab1df
aquilesTrindade <sempedra075@gmail.com>
2024-06-15 19:56:38 -0300

    fix: url in home.js

--- website/trindade/dev/js/oak/home/home.js
+++ website/trindade/dev/js/oak/home/home.js
@@ -1,6 +1,6 @@
-const baseUrl = "https://oak-store.github.io/Oak-Web/aglsk/pages/details/?project_dev_name="
+const baseUrl = "https://oak-store.github.io/Oak-Web/aglsk/pages/details/"
 var appID = ""
 
 function detailsApp(pName, dName) {
-  window.location.href = baseUrl + dName + "&project_name=" + pName
+  window.location.href = baseUrl + dName + "/" + pName
 }
